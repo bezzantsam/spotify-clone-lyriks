3b9c1a57172f020af7dd0eb35c9e889d55fd1450
Sam <bezzantsam@gmail.com>
2022-10-05 06:43:55 -0700

    Discover basic

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,17 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+  console.log(genres);
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col items-center justify-between w-full mt-4 mb-10 sm:flex-row">
+        <h2 className="text-3xl font-bold text-left text-white">Discover</h2>
+        <select></select>
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
