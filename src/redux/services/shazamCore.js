b49a46e6dfb656c86008a62e6c281e1db15ce613
Sam <bezzantsam@gmail.com>
2022-10-12 15:36:43 -0700

    breKPOINT

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,11 +1,20 @@
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "a1eed637aemsh58e64b984be8593p1b48fcjsn655d3219ab7f",
-    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
-  },
-};
-fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'a1eed637aemsh58e64b984be8593p1b48fcjsn655d3219ab7f');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: '/charts/world'}),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
